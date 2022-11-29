bc9f2d17882325aab89f6d0cb4424633a253c6aa
muhammed kocabas <75855099+ChrisMKocabas@users.noreply.github.com>
2022-11-29 10:07:25 -0500

    Last commit for submission

--- db.js
+++ db.js
@@ -1,3 +1,15 @@
+/******************************************************************************
+***
+* ITE5315 – Project
+* I declare that this assignment is my own work in accordance with Humber Academic Policy.
+* No part of this assignment has been copied manually or electronically from any other source
+* (including web sites) or distributed to other students.
+*
+* Group member Name: Muhammed Kocabas - Tatiana Trofimcuk 
+Student IDs: N01475765 N01490818 Date: 11-29-2022
+******************************************************************************
+***/
+
 var mongoose = require("mongoose");
 var database = require("./config/database");
 const restaurant = require("./models/restaurant");
@@ -72,8 +84,8 @@ async function getAllRestaurants(
         console.log("Required parameters missing.", err);
       }
     }
-  } catch {
-    console.log("there was a problem");
+  } catch (err) {
+    console.log("there was a problem. Unable to get and restaurants", err);
   }
 }
 
