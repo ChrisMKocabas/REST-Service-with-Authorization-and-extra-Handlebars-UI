9ba64994687ef9d62b93ea38bb0d11f31fdf47ce
ChrisMKocabas <muhammedkocabas@outlook.com>
2023-03-08 17:20:42 -0500

    Security settings updated

--- config/database.js
+++ config/database.js
@@ -1,13 +1,3 @@
-/******************************************************************************
-***
-* ITE5315 – Project
-* I declare that this assignment is my own work in accordance with Humber Academic Policy.
-* No part of this assignment has been copied manually or electronically from any other source
-* (including web sites) or distributed to other students.
-*
-* Group member Name: Muhammed Kocabas - Tatiana Trofimcuk 
-Student IDs: N01475765 N01490818 Date: 11-29-2022
-******************************************************************************
-***/
+
 
 // DATA MOVED TO .env FOR SECURITY PURPOSES
